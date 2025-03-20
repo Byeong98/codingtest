dd369da4407d41fd42313d453c5d6e882f670709
김병민 <gimbyeongmin@byeong.local>
2025-03-20 19:21:45 +0900

    복습: 카드뭉치

--- lv01/07.py
+++ lv01/07.py
@@ -58,5 +58,18 @@ def solution(cards1, cards2, goal):
     return 'Yes'            
 
 
-print(solution(["i", "drink", "water"],["want", "to"],["i", "want", "to", "drink", "water"]))
-print(solution(["i", "water", "drink"],["want", "to"],["i", "want", "to", "drink", "water"]))
+
+
+
+def solution2(cards1, cards2, goal):
+    for i in goal:
+        if cards1 and i == cards1[0]: 
+            cards1.pop(0)
+        elif cards2 and i == cards2[0]:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
+
+print(solution2(["i", "drink", "water"],["want", "to"],["i", "want", "to", "drink", "water"]))
+print(solution2(["i", "water", "drink"],["want", "to"],["i", "want", "to", "drink", "water"]))
